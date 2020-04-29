62aceeb083d00b818b453186597ddc4cb8f59978
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2020-04-29 16:09:39 +0530

    India cases state wise ui feature added

---
+++ app/src/main/java/com/shubham/covid_19track/India/SelectState.kt
@@ -0,0 +1,13 @@
+package com.shubham.covid_19track.India
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shubham.covid_19track.R
+
+class SelectState : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_state)
+    }
+}
---
+++ app/src/main/java/com/shubham/covid_19track/India/StateWiseReport.kt
@@ -0,0 +1,13 @@
+package com.shubham.covid_19track.India
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shubham.covid_19track.R
+
+class StateWiseReport : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_state_wise_report)
+    }
+}
