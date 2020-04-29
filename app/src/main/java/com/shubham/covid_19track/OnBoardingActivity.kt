079fd34d7894b5a46b0ecfbbbe6ee10cedd06ac1
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2020-04-29 16:13:09 +0530

    ui connected with boarding activity

--- app/src/main/java/com/shubham/covid_19track/OnBoardingActivity.kt
+++ app/src/main/java/com/shubham/covid_19track/OnBoardingActivity.kt
@@ -3,6 +3,7 @@ package com.shubham.covid_19track
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shubham.covid_19track.India.DashBoardActivity
 import kotlinx.android.synthetic.main.activity_on_boarding.*
 
 class OnBoardingActivity : AppCompatActivity() {
@@ -14,6 +15,10 @@ class OnBoardingActivity : AppCompatActivity() {
         world_layout.setOnClickListener {
             startActivity(Intent(applicationContext, MainActivity::class.java))
 
+        }
+
+        india_layout.setOnClickListener {
+            startActivity(Intent(applicationContext, DashBoardActivity::class.java))
         }
     }
 }
