c990c61810e97db276513b1a745149ca5234e814
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2020-06-09 16:38:29 +0530

    progress bar added in webView

---
+++ app/src/main/java/com/shubham/covid_19track/India_Govt_WebActivity.java
@@ -0,0 +1,44 @@
+package com.shubham.covid_19track;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
+
+public class India_Govt_WebActivity extends AppCompatActivity {
+
+    WebView webview;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.web_view);
+
+        webview = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.progressBar);
+        webview.setWebViewClient(new India_Govt_WebActivity.WebViewClient());
+        webview.loadUrl("https://www.mohfw.gov.in/");
+
+    }
+
+    public class WebViewClient extends android.webkit.WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+}
