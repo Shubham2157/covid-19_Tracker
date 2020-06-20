a4e558c7b8f73a04513cff55de6d46e8b2fe8c0a
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2020-06-20 14:11:31 +0530

    Today cases and test report code clean

--- app/src/main/java/com/shubham/covid_19track/India/TestsReport.kt
+++ app/src/main/java/com/shubham/covid_19track/India/TestsReport.kt
@@ -4,21 +4,13 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.shubham.covid_19track.R
+import kotlinx.android.synthetic.main.activity_tests_report.*
 
 class TestsReport : AppCompatActivity() {
-
-    private var txt_total_tests_rep: TextView? = null
-    private  var txt_positive_cas:TextView? = null
-    private  var txt_updated_tes:TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tests_report)
-
-        txt_positive_cas = findViewById(R.id.txt_positive_cas)
-        txt_total_tests_rep = findViewById(R.id.txt_total_tests_rep)
-        txt_updated_tes = findViewById(R.id.txt_updated_tes)
-
 
         val positiveCases = intent.getStringExtra("totalPositive")
         val totalTests = intent.getStringExtra("totalIndividuals")
--- app/src/main/java/com/shubham/covid_19track/India/TodayCases.kt
+++ app/src/main/java/com/shubham/covid_19track/India/TodayCases.kt
@@ -4,23 +4,13 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.shubham.covid_19track.R
+import kotlinx.android.synthetic.main.activity_today_cases.*
 
 class TodayCases : AppCompatActivity() {
-
-    private var txt_confirmed_today: TextView? = null
-    private  var txt_deaths_tod:TextView? = null
-    private  var txt_recovered_tod:TextView? = null
-    private  var txt_updated_today:TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_today_cases)
-
-        txt_confirmed_today = findViewById(R.id.txt_confirmed_today)
-        txt_deaths_tod = findViewById(R.id.txt_deaths_tod)
-        txt_recovered_tod = findViewById(R.id.txt_recovered_tod)
-        txt_updated_today = findViewById(R.id.txt_updated_today)
-
 
         val confirmed = intent.getStringExtra("confirmed")
         val deaths = intent.getStringExtra("deaths")
